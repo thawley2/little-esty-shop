d5efd9a6ae021b39f3e8c043df9cf9bf55e9f392
Thomas Hawley <thawley22@turing.edu>
2023-04-12 17:39:43 -0600

    test: Add test for merchant dashboard top 5 customers

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -9,6 +9,10 @@ RSpec.describe 'Merchant Show Dashboard Page', type: :feature do
   let!(:merchant1) {Merchant.create!(name:'Steve')}
   let!(:merchant2) {Merchant.create!(name:'Fred')}
 
+  before(:each) do
+    test_data
+  end
+
   describe "As a merchant visiting '/merchants/:id/dashboard'" do
     it 'I see the name of my merchant' do
       visit merchant_dashboard_path(merchant1)
@@ -30,6 +34,19 @@ RSpec.describe 'Merchant Show Dashboard Page', type: :feature do
       expect(page).to have_link('My Invoices')
       click_link("My Invoices")
       expect(current_path).to eq(merchant_invoices_path(merchant1))
+    end
+
+    describe 'shows the names of the top 5 customers' do
+      it 'who have conducted the largest number of successful transactions with my merchant' do
+        visit merchant_dashboard_path(@merchant)
+        require 'pry'; binding.pry
+        expect(page).to have_content('Favorite Customers')
+        expect(page).to have_content("1. #{@customer1.first_name} #{@customer1.last_name} - 4 purchases")
+        expect(page).to have_content("2. #{@customer2.first_name} #{@customer2.last_name} - 3 purchases")
+        expect(page).to have_content("3. #{@customer3.first_name} #{@customer3.last_name} - 2 purchases")
+        expect(page).to have_content("4. #{@customer4.first_name} #{@customer4.last_name} - 1 purchases")
+        expect(page).to have_content("5. #{@customer5.first_name} #{@customer5.last_name} - 1 purchases")
+      end
     end
   end
 end
