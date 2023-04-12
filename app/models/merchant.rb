90c015ca71c95c8e110f21a898146c2f1775611d
Thomas Hawley <thawley22@turing.edu>
2023-04-12 17:40:40 -0600

    update: Add additional relationships to models

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,9 @@
 class Merchant < ApplicationRecord
   has_many :items
+  has_many :invoices, through: :items
+  has_many :customers, through: :invoices
+
+  def top_five_customers
+    customers.joins(:transactions).group(:id)
+  end
 end
