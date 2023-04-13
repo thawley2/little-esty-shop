59df0c3066a8d717a9e99b6cb83295d84f41dd75
Nick Sacco <nicksacco10@gmail.com>
2023-04-13 09:00:49 -0700

    test: items ready to ship

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -39,7 +39,7 @@ RSpec.describe 'Merchant Show Dashboard Page', type: :feature do
     describe 'shows the names of the top 5 customers' do
       it 'who have conducted the largest number of successful transactions with my merchant' do
         visit merchant_dashboard_path(@merchant)
-    
+        
         expect(page).to have_content('Favorite Customers')
         expect(page).to have_content("#{@customer6.first_name} #{@customer6.last_name} - 4 purchases")
         expect(page).to have_content("#{@customer2.first_name} #{@customer2.last_name} - 3 purchases")
@@ -47,12 +47,32 @@ RSpec.describe 'Merchant Show Dashboard Page', type: :feature do
         expect(page).to have_content("#{@customer4.first_name} #{@customer4.last_name} - 2 purchases")
         expect(page).to have_content("#{@customer5.first_name} #{@customer5.last_name} - 2 purchases")
         expect(page).to_not have_content("#{@customer1.first_name} #{@customer1.last_name}")
-
+        
         expect(@customer6.first_name).to appear_before(@customer2.first_name)
         expect(@customer2.first_name).to appear_before(@customer3.first_name)
         expect(@customer3.first_name).to appear_before(@customer4.first_name)
         expect(@customer4.first_name).to appear_before(@customer5.first_name)
         expect(@customer5.first_name).to_not appear_before(@customer2.first_name)
+      end
+    end
+    
+    describe "I see a section for 'Items Ready to Ship'" do
+      describe 'shows a list of the names of all of my items that have been ordered and have not yet been shipped,' do
+        it "Next to each Item I see the id of the invoice that ordered my item And each invoice id is a link to my merchant's invoice show page" do
+          visit merchant_dashboard_path(@merchant)
+
+          expect(page).to have_content("Items Ready To Ship")
+          expect(page).to have_content("#{@item1.name} - Invoice ##{@invoice2.id}")
+          expect(page).to have_content("#{@item2.name} - Invoice ##{@invoice3.id}")
+          expect(page).to have_content("#{@item2.name} - Invoice ##{@invoice4.id}")
+          expect(page).to have_content("#{@item3.name} - Invoice ##{@invoice5.id}")
+          expect(page).to_not have_content("#{@item3.name} - Invoice ##{@invoice6.id}")
+          expect(page).to_not have_content("#{@item1.name} - Invoice ##{@invoice1.id}")
+          expect(page).to have_link(@invoice2.id)
+          expect(page).to have_link(@invoice3.id)
+          expect(page).to have_link(@invoice4.id)
+          expect(page).to have_link(@invoice5.id)
+        end
       end
     end
   end
