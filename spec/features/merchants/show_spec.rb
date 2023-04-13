f909572c8aa6c55839883ae138b8304303d241ea
Thomas Hawley <thawley22@turing.edu>
2023-04-12 21:33:06 -0600

    test: Add test to merchant dashboard to display top 5 customers

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -39,13 +39,20 @@ RSpec.describe 'Merchant Show Dashboard Page', type: :feature do
     describe 'shows the names of the top 5 customers' do
       it 'who have conducted the largest number of successful transactions with my merchant' do
         visit merchant_dashboard_path(@merchant)
-        require 'pry'; binding.pry
+    
         expect(page).to have_content('Favorite Customers')
-        expect(page).to have_content("1. #{@customer1.first_name} #{@customer1.last_name} - 4 purchases")
-        expect(page).to have_content("2. #{@customer2.first_name} #{@customer2.last_name} - 3 purchases")
-        expect(page).to have_content("3. #{@customer3.first_name} #{@customer3.last_name} - 2 purchases")
-        expect(page).to have_content("4. #{@customer4.first_name} #{@customer4.last_name} - 1 purchases")
-        expect(page).to have_content("5. #{@customer5.first_name} #{@customer5.last_name} - 1 purchases")
+        expect(page).to have_content("#{@customer6.first_name} #{@customer6.last_name} - 4 purchases")
+        expect(page).to have_content("#{@customer2.first_name} #{@customer2.last_name} - 3 purchases")
+        expect(page).to have_content("#{@customer3.first_name} #{@customer3.last_name} - 2 purchases")
+        expect(page).to have_content("#{@customer4.first_name} #{@customer4.last_name} - 2 purchases")
+        expect(page).to have_content("#{@customer5.first_name} #{@customer5.last_name} - 2 purchases")
+        expect(page).to_not have_content("#{@customer1.first_name} #{@customer1.last_name}")
+
+        expect(@customer6.first_name).to appear_before(@customer2.first_name)
+        expect(@customer2.first_name).to appear_before(@customer3.first_name)
+        expect(@customer3.first_name).to appear_before(@customer4.first_name)
+        expect(@customer4.first_name).to appear_before(@customer5.first_name)
+        expect(@customer5.first_name).to_not appear_before(@customer2.first_name)
       end
     end
   end
