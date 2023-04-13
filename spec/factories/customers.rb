231b3221e91b6f1d75b148f32ef7f02c71cc6924
Amy Spears <amspears007@gmail.com>
2023-04-13 16:52:43 -0600

    Remove spec factory traits

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -5,18 +5,4 @@ FactoryBot.define do
     created_at { Faker::Time.between(from: 2.years.ago, to: Time.now) }
     updated_at { created_at }
   end
-
-  trait :with_successful_transactions do
-    transient do
-      transactions_count { 1 }
-      transactions_traits { [:successful] }
-    end
-
-    after(:create) do |customer, evaluator|
-      create_list(:transaction, 
-        evaluator.transactions_count, 
-        *evaluator.transactions_traits, 
-        invoice: create(:invoice, customer: customer))
-    end
-  end
 end
--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -7,8 +7,4 @@ FactoryBot.define do
     created_at { Faker::Time.between(from: 2.years.ago, to: Time.now) }
     updated_at { created_at }
   end
-
-  trait :successful do
-    result { "success" }
-  end
 end
