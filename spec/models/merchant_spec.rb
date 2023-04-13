6eb8bd7a9a8e38e94c3647ce56da9e3a58da86a3
Thomas Hawley <thawley22@turing.edu>
2023-04-13 17:05:56 -0600

    test: Update tests for robustness

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -5,10 +5,21 @@ RSpec.describe Merchant, type: :model do
     test_data
   end
   it { should have_many(:items) }
+  it { should have_many(:invoice_items).through(:items)}
+  it { should have_many(:invoices).through(:items)}
+  it { should have_many(:customers).through(:invoices)}
 
   describe 'instance methods' do
     it '#top_5_customers' do
       expect(@merchant.top_5_customers).to eq([@customer6, @customer2, @customer3, @customer4, @customer5])
+    end
+    
+    it 'has the attribute of transaction_count' do
+      expect(@merchant.top_5_customers.first.transaction_count).to eq(4)
+      expect(@merchant.top_5_customers[1].transaction_count).to eq(3)
+      expect(@merchant.top_5_customers[2].transaction_count).to eq(2)
+      expect(@merchant.top_5_customers[3].transaction_count).to eq(2)
+      expect(@merchant.top_5_customers[4].transaction_count).to eq(2)
     end
 
     it '#items_not_shipped' do
