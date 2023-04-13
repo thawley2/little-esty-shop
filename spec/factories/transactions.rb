c56e7502d8895b70d2661513c6d49fb47edaf1db
Amy Spears <amspears007@gmail.com>
2023-04-13 12:55:07 -0600

    Updated facotry data

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -7,4 +7,8 @@ FactoryBot.define do
     created_at { Faker::Time.between(from: 2.years.ago, to: Time.now) }
     updated_at { created_at }
   end
+
+  trait :successful do
+    result { "success" }
+  end
 end
