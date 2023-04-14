4b7d6093649e1747c427682ffe5ac4f9ac0c509d
Thomas Hawley <thawley22@turing.edu>
2023-04-14 15:56:58 -0600

    Add test for item name links

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe '/merchants/:id/items' do
+RSpec.describe '/merchants/:id/items', type: :feature do
   before(:each) do
     test_data
-    merchant2
+    merchant2_test_data
   end
   describe 'When I visit my merchant items index page' do
     it 'I see a list of the names of all of my items and not other merchant items' do
@@ -21,6 +21,22 @@ RSpec.describe '/merchants/:id/items' do
       expect(page).to_not have_content(@item8.name)
       expect(page).to_not have_content(@item9.name)
       expect(page).to_not have_content(@item10.name)
+    end
+
+    describe 'When I click on the name of an item from the merchant items index page' do
+      it 'Then I am taken to that merchants items show page' do
+        visit merchant_items_path(@merchant)
+
+      expect(page).to have_link("#{@item1.name}")
+      expect(page).to have_link("#{@item2.name}")
+      expect(page).to have_link("#{@item3.name}")
+      expect(page).to have_link("#{@item4.name}")
+      expect(page).to have_link("#{@item5.name}")
+
+      click_link("#{@item1.name}")
+
+      expect(current_path).to eq(merchant_item_path(@merchant, @item1))
+      end
     end
   end
 end
