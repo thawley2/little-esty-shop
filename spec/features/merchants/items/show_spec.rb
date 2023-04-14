873d31fffd7718b18dadaffe04318187863de9ae
Thomas Hawley <thawley22@turing.edu>
2023-04-14 16:02:08 -0600

    test: Add test for merchant/item/show page

---
+++ spec/features/merchants/items/show_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe '/merchants/:merchant_id/items/:id', type: :feature do
+  before(:each) do
+    test_data
+    merchant2_test_data
+  end
+
+  describe 'When I visit the merchant item show page' do
+    it 'shows all the items attributes (name, description, current selling price)' do
+      @item1.update(unit_price: 15000)
+      visit merchant_item_path(@merchant, @item1)
+save_and_open_page
+      expect(page).to have_content("Little Esty Shop")
+      expect(page).to have_content(@merchant.name)
+      expect(page).to have_content(@item1.name)
+      expect(page).to have_content("Description: #{@item1.description}")
+      expect(page).to have_content("Current Price: $150.00")
+    end
+  end
+end
