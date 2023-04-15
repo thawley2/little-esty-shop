647283836cb0d5f3d347ed10cabb28ecfc6ea5f7
Nick Sacco <nicksacco10@gmail.com>
2023-04-14 21:18:33 -0700

    feat wip: merchant items controller update action

--- app/controllers/merchants/items_controller.rb
+++ app/controllers/merchants/items_controller.rb
@@ -12,4 +12,17 @@ class Merchants::ItemsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @item = Item.find(params[:id])
   end
+  
+  def update
+    @merchant = Merchant.find(params[:id])
+    @item = Item.find(params[:id])
+
+    if @item.update(item_params)
+      flash[:success] = "#{@item.name} was successfully updated."
+      redirect_to merchant_item_path(@merchant, @item)
+    else
+      flash[:error] = "You messed up. Please try again"
+      render :edit
+    end
+  end
 end
