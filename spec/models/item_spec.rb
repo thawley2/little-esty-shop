aba92779f445daca8f3f6f84821b1dd08f5e5775
Thomas Hawley <thawley22@turing.edu>
2023-04-15 15:59:28 -0600

    test: Add test for status enum

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,4 +4,5 @@ RSpec.describe Item, type: :model do
   it { should belong_to(:merchant) }
   it { should have_many(:invoice_items) }
   it { should have_many(:invoices).through(:invoice_items) }
+  it { should define_enum_for(:status).with_values('enabled': 1, 'disabled': 0) }
 end
