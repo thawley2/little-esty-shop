a0f14e732b90898b13ef8435af8254d6c88180e3
Andrew Bowman <andrewtbowman@gmail.com>
2023-04-15 19:41:04 -0400

    undo merchant @ refactor

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -12,16 +12,16 @@ class Admin::MerchantsController < ApplicationController
   end
 
   def update
-    merchant = Merchant.find(params[:id])
+    @merchant = Merchant.find(params[:id])
 
     if params[:switch_enabled]
-      merchant.switch_enabled
-      enabled_text = merchant.enabled ? "Enabled" : "Disabled"
-      redirect_to admin_merchants_path, notice: "#{merchant.name} is #{enabled_text}"
+      @merchant.switch_enabled
+      enabled_text = @merchant.enabled ? "Enabled" : "Disabled"
+      redirect_to admin_merchants_path, notice: "#{@merchant.name} is #{enabled_text}"
 
-    elsif merchant.update(merchant_params)
-      flash[:success] = "#{merchant.name} was successfully updated."
-      redirect_to admin_merchant_path(merchant)
+    elsif @merchant.update(merchant_params)
+      flash[:success] = "#{@merchant.name} was successfully updated."
+      redirect_to admin_merchant_path(@merchant)
 
     else
       flash[:error] = "Error updating merchant. Please try again."
