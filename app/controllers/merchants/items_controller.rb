80d457569ce2961e7b8e208e14c4d210f0fb06b9
Nick Sacco <nicksacco10@gmail.com>
2023-04-14 19:30:14 -0700

    feat: add edit action to merchant item controller

--- app/controllers/merchants/items_controller.rb
+++ app/controllers/merchants/items_controller.rb
@@ -7,4 +7,8 @@ class Merchants::ItemsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @item = Item.find(params[:id])
   end
+
+  def edit
+    
+  end
 end
