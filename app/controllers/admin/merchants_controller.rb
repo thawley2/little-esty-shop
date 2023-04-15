d3d67a167a96d896a9b779aef4e39338692c1e0e
Amy Spears <amspears007@gmail.com>
2023-04-14 21:13:31 -0600

    Add update method to controller with correct alias route

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -9,11 +9,17 @@ class Admin::MerchantsController < ApplicationController
 
   def edit
     @merchant = Merchant.find(params[:id])
-
   end
 
+  def update
+    merchant = Merchant.find(params[:id])
+    merchant.update(merchant_params)
+    redirect_to admin_merchant_path(merchant)
+  end
+  
+
   private
-    def merchant_params
-      params.require(:merchant).permit(:name)
-    end
+  def merchant_params
+    params.require(:merchant).permit(:name)
+  end
 end
