e4b599a77b36d54041a7bf75519b7e2a86501513
Amy Spears <amspears007@gmail.com>
2023-04-14 21:28:16 -0600

    Update update methdo to have if/else for flash method

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -12,9 +12,15 @@ class Admin::MerchantsController < ApplicationController
   end
 
   def update
-    merchant = Merchant.find(params[:id])
-    merchant.update(merchant_params)
-    redirect_to admin_merchant_path(merchant)
+    @merchant = Merchant.find(params[:id])
+
+    if @merchant.update(merchant_params)
+      flash[:success] = "#{@merchant.name} was successfully updated."
+      redirect_to admin_merchant_path(@merchant)
+    else
+      flash[:error] = "Error updating merchant. Please try again."
+      render :edit
+    end
   end
   
 
