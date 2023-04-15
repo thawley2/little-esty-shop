d3458e3e6fb1bbc3e8a01e17915aff75e6c39564
Amy Spears <amspears007@gmail.com>
2023-04-15 14:18:32 -0600

    Add patch route so helper method can be accessed

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,11 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :invoices, only: [:show]
-    resources :merchants, only: [:index, :show, :edit, :update], controller: 'merchants'
+    resources :merchants, only: [:index, :show, :edit, :update], controller: 'merchants' do
+      member do
+        patch :switch_enabled
+        
+      end
+    end
   end
 end
