e0bfdfc8c54d7e8934f4acd6a3ce14744583455f
Andrew Bowman <andrewtbowman@gmail.com>
2023-04-15 19:24:39 -0400

    refactor ternary

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -16,7 +16,7 @@ class Admin::MerchantsController < ApplicationController
 
     if params[:switch_enabled]
       @merchant.switch_enabled
-      enabled_status = @merchant.enabled? ? "Enabled" : "Disabled"
+      enabled_status = @merchant.enabled ? "Enabled" : "Disabled"
       redirect_to admin_merchants_path, notice: "#{@merchant.name} is #{enabled_status}"
 
     elsif @merchant.update(merchant_params)
