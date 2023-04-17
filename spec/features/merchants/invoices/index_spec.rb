28a90eb21959b3f4c8a4a04a522c5005bde67055
Nick Sacco <nicksacco10@gmail.com>
2023-04-16 17:16:26 -0700

    test: merchant invoices index tests

--- spec/features/merchants/invoices/index_spec.rb
+++ spec/features/merchants/invoices/index_spec.rb
@@ -2,14 +2,16 @@ require 'rails_helper'
 
 RSpec.describe '/merchants/merchant_id/invoices', type: :feature do
   before(:each) do
-    test_data
     merchant2_test_data
   end
   describe "When I visit my merchant's invoices index page" do
     it "Then I see all of the invoices that include at least one of my merchant's items" do
-      
-    end
+      visit merchant_invoices_path(@merchant)
 
-    xit "For each invoice I see its id and each id links to the merchant invoice show page" do
+      expect(page).to have_content("Little Esty Shop")
+      expect(page).to have_content(@merchant.name)
+      expect(page).to have_content("My Invoices")
+      expect(page).to have_content("Invoice #{@invoice.id}")
+    end
   end
 end
