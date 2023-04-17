1f568fc423188ba1bfc426437209c526655afd01
Nick Sacco <nicksacco10@gmail.com>
2023-04-16 20:12:48 -0700

    test: complete us15 tests

--- spec/features/merchants/invoices/show_spec.rb
+++ spec/features/merchants/invoices/show_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe '/merchants/merchant_id/invoices/invoice_id)', type: :feature do
       expect(page).to have_content(@invoice7.status)
       expect(page).to have_content("Created on: #{@invoice7.created_at.strftime("%B/%d/%Y")}")
       expect(page).to have_content("Customer:")
-      expect(page).to have_content("#{customer7.full_name}")
+      expect(page).to have_content("#{@customer7.full_name}")
     end
   end
 end
