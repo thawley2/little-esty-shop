65900f457a7ae69a85aadef66593b381758508e3
Nick Sacco <nicksacco10@gmail.com>
2023-04-16 20:16:54 -0700

    test: add testing for additonal merchant invoices

--- spec/features/merchants/invoices/show_spec.rb
+++ spec/features/merchants/invoices/show_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe '/merchants/merchant_id/invoices/invoice_id)', type: :feature do
   before(:each) do
+    test_data
     merchant2_test_data
   end
   describe "When I visit my merchant's invoice show page" do
@@ -15,6 +16,16 @@ RSpec.describe '/merchants/merchant_id/invoices/invoice_id)', type: :feature do
       expect(page).to have_content("Created on: #{@invoice7.created_at.strftime("%B/%d/%Y")}")
       expect(page).to have_content("Customer:")
       expect(page).to have_content("#{@customer7.full_name}")
+
+      visit merchant_invoice_path(@merchant, @invoice1)
+
+      expect(page).to have_content("Little Esty Shop")
+      expect(page).to have_content(@merchant.name)
+      expect(page).to have_content(@invoice1.id)
+      expect(page).to have_content(@invoice1.status)
+      expect(page).to have_content("Created on: #{@invoice1.created_at.strftime("%B/%d/%Y")}")
+      expect(page).to have_content("Customer:")
+      expect(page).to have_content("#{@customer1.full_name}")
     end
   end
 end
