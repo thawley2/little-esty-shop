9347695478d6e94501834dd209980da3a18c5d1e
Andrew Bowman <andrewtbowman@gmail.com>
2023-04-18 17:13:21 -0400

    test: us34 admin invoice invoice_item data

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -10,5 +10,20 @@ RSpec.describe 'Invoice Index' do
       expect(page).to have_content(@invoice1.status)
       expect(page).to have_content(@invoice1.customer.full_name)
     end
+
+    it 'US34 I see all of the items on the invoice with their data' do
+      test_data
+      visit admin_invoice_path(@invoice1)
+
+      expect(page).to have_content(@inv_itm1.item.name)
+      expect(page).to have_content(@inv_itm1.quantity)
+      expect(page).to have_content("Price: $#{@inv_itm1.unit_price / 100.0}")
+      expect(page).to have_content(@inv_itm1.status)
+    end
   end
 end
+
+# Item name
+# The quantity of the item ordered
+# The price the Item sold for
+# The Invoice Item status
