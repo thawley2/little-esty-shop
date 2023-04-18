fcdf2fe02786cfcc6caeb6b9b313814e8a724f5a
Amy Spears <amspears007@gmail.com>
2023-04-17 20:08:15 -0600

    Test: remvoed test line that was causing bug

--- spec/models/merchant_top5_spec.rb
+++ spec/models/merchant_top5_spec.rb
@@ -22,12 +22,14 @@ RSpec.describe Merchant, type: :model do
     it 'returns the top five merchants based on total revenue in descending order' do
       top_merchants = Merchant.top_five
 
-      expect(top_merchants.count).to eq(5)
       expect(top_merchants[0]).to eq(merchant6)
       expect(top_merchants[1]).to eq(merchant5)
       expect(top_merchants[2]).to eq(merchant4)
       expect(top_merchants[3]).to eq(merchant3)
       expect(top_merchants[4]).to eq(merchant2)
+      expect(top_merchants).to match_array([merchant6, merchant5, merchant4, merchant3, merchant2])
+
+      expect(Merchant.top_five.length).to eq(5)
     end
   end
 
