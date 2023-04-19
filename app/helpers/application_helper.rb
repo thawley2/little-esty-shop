b319fe957f1f5cabc45d6ef123ee3635d9e7c424
Nick Sacco <nicksacco10@gmail.com>
2023-04-19 07:46:10 -0700

    feat wip: logo api on every page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def unsplash_app_logo_url
+    response = HTTParty.get('https://api.unsplash.com/photos/random/?client_id=1cd3V-4rbtUVO3VE00Xus_BcKE95xqwK4sdttc4GFsY')
+    json = JSON.parse(response.body)
+    json['urls']['thumb']
+    # require 'pry'; binding.pry
+  end
 end
