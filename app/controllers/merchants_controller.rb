d47729efac5ba278abf9fc6d16b51c9fb105f3f7
Thomas Hawley <thawley22@turing.edu>
2023-04-19 16:20:06 -0600

    Add merchant image to merchant dashboard

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,5 +1,6 @@
 class MerchantsController < ApplicationController
   def show
     @merchant = Merchant.find(params[:id])
+    @merchant_image = MerchantSearch.merchant_info(@merchant.name)
   end
 end
