43e4ec33540492ce42c07bb2dd4abb405d75709b
Thomas Hawley <thawley22@turing.edu>
2023-04-19 16:26:31 -0600

    render setup

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe 'Merchant Show Dashboard Page', type: :feature do
   describe "As a merchant visiting '/merchants/:id/dashboard'" do
     it 'I see the name of my merchant' do
       visit merchant_dashboard_path(merchant1)
-      save_and_open_page
+      
       expect(page).to have_content('Steve')
     end
     
