d1d7d11df404452dbb900905723fb1891494153f
Thomas Hawley <thawley22@turing.edu>
2023-04-21 10:31:24 -0600

    feat: Update merchant model with has many bulk_discounts, create bulk_discount model and tests, set relationship for bulk_discount to belongs_to merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,7 +3,7 @@ class Merchant < ApplicationRecord
   has_many :invoice_items, through: :items
   has_many :invoices, through: :items
   has_many :customers, through: :invoices
-  has_many :transactions, through: :invoices
+  has_many :bulk_discounts
 
   def top_5_customers
     self.customers.joins(:transactions)
