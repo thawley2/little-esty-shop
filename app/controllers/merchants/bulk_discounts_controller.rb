23074eb95012ac55e4cd282c868321e382be1952
Thomas Hawley <thawley22@turing.edu>
2023-04-21 19:29:43 -0600

    feat: Add new action to controller

--- app/controllers/merchants/bulk_discounts_controller.rb
+++ app/controllers/merchants/bulk_discounts_controller.rb
@@ -7,4 +7,9 @@ class Merchants::BulkDiscountsController < ApplicationController
   def show
     @discount = BulkDiscount.find(params[:id])
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = BulkDiscount.new
+  end
 end
