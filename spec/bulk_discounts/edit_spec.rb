0fcb042fd773ff2d45dfa242356203d54358fab7
Thomas Hawley <thawley22@turing.edu>
2023-04-22 14:34:19 -0600

    test: Add test for edit form along with sad path testing

--- spec/bulk_discounts/edit_spec.rb
+++ spec/bulk_discounts/edit_spec.rb
@@ -10,16 +10,82 @@ RSpec.describe '/bulk_discounts/:id/edit', type: :feature do
       visit edit_bulk_discount_path(@discount1)
 
       expect(page).to have_content("Edit #{@discount1.name} Discount")
-      expect(page).to have_field("Discount Name")
-      expect(page).to have_field("Percent Discount range:(0.05-1.00)")
-      expect(page).to have_field("Quantity Threshold")
-      expect(page.has_field?("Discount Name", with: @discount1.name)).to be true
-      expect(page.has_field?("Percent Discount range:(0.05-1.00)", with: @discount1.percent_discount)).to be true
-      expect(page.has_field?("Quantity Threshold", with: @discount1.quantity_threshold)).to be true
+      expect(page).to have_field("Discount Name", with: @discount1.name)
+      expect(page).to have_field("Percent Discount range:(0.05-1.00)", with: @discount1.percent_discount)
+      expect(page).to have_field("Quantity Threshold", with: @discount1.quantity_threshold)
       expect(page).to have_button('Update Bulk discount')
 
     end
 
-    it 'When I '
+    it 'When I change a value in a field and click update I am taken back to the discounts show page and see the updated info' do
+      visit edit_bulk_discount_path(@discount1)
+
+      fill_in 'Discount Name', with: 'Blast Off'
+
+      click_button("Update Bulk discount")
+
+      expect(current_path).to eq(bulk_discount_path(@discount1))
+      expect(page).to have_content('Blast Off discount')
+
+      visit edit_bulk_discount_path(@discount1)
+
+      fill_in 'Percent Discount range:(0.05-1.00)', with: 0.1
+
+      click_button("Update Bulk discount")
+
+      expect(current_path).to eq(bulk_discount_path(@discount1))
+      expect(page).to have_content('Percent Discount: 10%')
+      
+      visit edit_bulk_discount_path(@discount1)
+
+      fill_in 'Quantity Threshold', with: 25
+
+      click_button("Update Bulk discount")
+
+      expect(current_path).to eq(bulk_discount_path(@discount1))
+      expect(page).to have_content('Quantity Threshold: 25')
+    end
+
+    it 'displays an error if a field is left blank when clicking Update' do
+      visit edit_bulk_discount_path(@discount1)
+
+      fill_in 'Quantity Threshold', with: ''
+
+      click_button("Update Bulk discount")
+
+      expect(page).to have_content('Please do not leave any fields blank')
+      expect(page).to have_content("Edit #{@discount1.name} Discount")
+      expect(page).to have_field("Discount Name", with: @discount1.name)
+      expect(page).to have_field("Percent Discount range:(0.05-1.00)", with: @discount1.percent_discount)
+      expect(page).to have_field("Quantity Threshold")
+      expect(page).to have_button('Update Bulk discount')
+
+      fill_in 'Quantity Threshold', with: 25
+      click_button("Update Bulk discount")
+
+      expect(current_path).to eq(bulk_discount_path(@discount1))
+      expect(page).to have_content('Quantity Threshold: 25')
+    end
+
+    it 'displays an error if a number field is filled in with a string and clicking Update' do
+      visit edit_bulk_discount_path(@discount1)
+
+      fill_in 'Quantity Threshold', with: 'nothing to see here'
+
+      click_button("Update Bulk discount")
+
+      expect(page).to have_content('Please do not leave any fields blank')
+      expect(page).to have_content("Edit #{@discount1.name} Discount")
+      expect(page).to have_field("Discount Name", with: @discount1.name)
+      expect(page).to have_field("Percent Discount range:(0.05-1.00)", with: @discount1.percent_discount)
+      expect(page).to have_field("Quantity Threshold")
+      expect(page).to have_button('Update Bulk discount')
+
+      fill_in 'Quantity Threshold', with: 25
+      click_button("Update Bulk discount")
+
+      expect(current_path).to eq(bulk_discount_path(@discount1))
+      expect(page).to have_content('Quantity Threshold: 25')
+    end
   end
 end
