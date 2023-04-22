524f46ad45353a47fc66fb75dbdff2a92d2a6c55
Thomas Hawley <thawley22@turing.edu>
2023-04-22 14:34:57 -0600

    feat: Add update action and strong params method

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -7,6 +7,20 @@ before_action :find_discount
   def edit
   end
 
+  def update
+    if @discount.update(bulk_discount_params)
+      redirect_to bulk_discount_path(@discount)
+    else
+      flash[:error] = 'Please do not leave any fields blank'
+      render :edit
+    end
+  end
+
+  private
+  def bulk_discount_params
+    params.require(:bulk_discount).permit(:name, :percent_discount, :quantity_threshold)
+  end
+
   def find_discount
     @discount = BulkDiscount.find(params[:id])
   end
