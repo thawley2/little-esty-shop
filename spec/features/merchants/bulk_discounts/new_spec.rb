6ac1230a231dafeb0681e65981d540c34547cf80
Thomas Hawley <thawley22@turing.edu>
2023-04-21 22:06:04 -0600

    test: Add test for filling out new bulk discount form

--- spec/features/merchants/bulk_discounts/new_spec.rb
+++ spec/features/merchants/bulk_discounts/new_spec.rb
@@ -15,5 +15,17 @@ RSpec.describe "/merchants/:id/bulk_discounts/new#new", type: :feature do
       expect(page).to have_field("Quantity Threshold")
       expect(page).to have_button("Create Bulk discount")
     end
+
+    it 'When I fill in the form with valid data and click Create, I am taken back to the bulk discount index page and see the new discount' do
+      visit new_merchant_bulk_discount_path(@merchant)
+
+      fill_in 'Discount Name', with: 'Flash Deal'
+      fill_in 'Percent Discount range:(0.05-1.00)', with: 0.25
+      fill_in 'Quantity Threshold', with: 5
+
+      click_button 'Create Bulk discount'
+
+      expect(current_path).to eq(merchant_bulk_discounts_path(@merchant))
+    end
   end
 end
