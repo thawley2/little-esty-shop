3716b1e7be196b74b56624dbde9a932bfb0cd333
Thomas Hawley <thawley22@turing.edu>
2023-04-21 21:32:50 -0600

    test: Add test for new form

--- spec/features/merchants/bulk_discounts/new_spec.rb
+++ spec/features/merchants/bulk_discounts/new_spec.rb
@@ -9,7 +9,11 @@ RSpec.describe "/merchants/:id/bulk_discounts/new#new", type: :feature do
     it 'I see a form to add a new bulk discount' do
       visit new_merchant_bulk_discount_path(@merchant)
 
-      expect(page).to have_content("#{@merchant.name} New")
+      expect(page).to have_content("#{@merchant.name} New Discount")
+      expect(page).to have_field("Discount Name")
+      expect(page).to have_field("Percent Discount range:(0.05-1.00)")
+      expect(page).to have_field("Quantity Threshold")
+      expect(page).to have_button("Create Bulk discount")
     end
   end
 end
