77aefbc33bfdb299327b59f47c9b86ffd05ecb6d
Thomas Hawley <thawley22@turing.edu>
2023-04-22 13:10:52 -0600

    test: Add test for bulk discount show page

--- spec/features/merchants/bulk_discounts/show_spec.rb
+++ spec/features/merchants/bulk_discounts/show_spec.rb
@@ -8,8 +8,16 @@ RSpec.describe 'bulk_discounts/:id', type: :feature do
   describe 'When I visit a discounts show page' do
     it 'I see the discounts name' do
       visit bulk_discount_path(@discount1)
-
+      
       expect(page).to have_content("#{@discount1.name} discount")
+      expect(page).to have_content("Quantity Threshold: #{@discount1.quantity_threshold}")
+      expect(page).to have_content("Percent Discount: 30%")
+
+      visit bulk_discount_path(@discount2)
+
+      expect(page).to have_content("#{@discount2.name} discount")
+      expect(page).to have_content("Quantity Threshold: #{@discount2.quantity_threshold}")
+      expect(page).to have_content("Percent Discount: 20%")
     end
   end
 end
