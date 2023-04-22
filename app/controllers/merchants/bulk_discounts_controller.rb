0fb44ec68f9b7737811195aa04f2222ad30699b0
Thomas Hawley <thawley22@turing.edu>
2023-04-21 22:07:04 -0600

    feat: Add create action with sad path

--- app/controllers/merchants/bulk_discounts_controller.rb
+++ app/controllers/merchants/bulk_discounts_controller.rb
@@ -8,4 +8,19 @@ class Merchants::BulkDiscountsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @discount = BulkDiscount.new
   end
+
+  def create
+    merchant = Merchant.find(params[:merchant_id])
+    new = merchant.bulk_discounts.new(bulk_discount_params)
+    if new.save
+      redirect_to merchant_bulk_discounts_path(merchant)
+    else
+      flash[:error] = 'Please fill out all required fields'
+      render :new
+    end 
+  end
+
+  def bulk_discount_params
+    params.require(:bulk_discount).permit(:name, :percent_discount, :quantity_threshold)
+  end
 end
