08e1e02eafe1f31135a56d90431036419c4a2fcc
Thomas Hawley <thawley22@turing.edu>
2023-04-21 18:14:06 -0600

    feat: Add show action to bulk discounts controller

--- app/controllers/merchants/bulk_discounts_controller.rb
+++ app/controllers/merchants/bulk_discounts_controller.rb
@@ -3,4 +3,8 @@ class Merchants::BulkDiscountsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
   end
+
+  def show
+    @discount = BulkDiscount.find(params[:id])
+  end
 end
