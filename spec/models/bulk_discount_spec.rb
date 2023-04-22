e72c8342000cdaa19796a4553518cad4a46166d4
Thomas Hawley <thawley22@turing.edu>
2023-04-21 22:13:21 -0600

    test: Add validation testing

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe BulkDiscount, type: :model do
   it {should belong_to :merchant}
+
+  describe 'Validations' do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :percent_discount}
+    it {should validate_presence_of :quantity_threshold}
+  end
 end
