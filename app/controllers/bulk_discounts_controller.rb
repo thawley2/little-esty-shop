614b03e4c6391d965143a53781cd11b62f05f19b
Thomas Hawley <thawley22@turing.edu>
2023-04-22 13:26:10 -0600

    feat: Create edit action and add before_action (find_discount)

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -1,6 +1,13 @@
 class BulkDiscountsController < ApplicationController
+before_action :find_discount
 
   def show
+  end
+
+  def edit
+  end
+
+  def find_discount
     @discount = BulkDiscount.find(params[:id])
   end
 end
