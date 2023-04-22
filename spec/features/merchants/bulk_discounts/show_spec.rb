d08db9e987aaa2f42caf947a3f670dce16a9492c
Thomas Hawley <thawley22@turing.edu>
2023-04-21 20:38:09 -0600

    refactor: Update link to visit path

--- spec/features/merchants/bulk_discounts/show_spec.rb
+++ spec/features/merchants/bulk_discounts/show_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe '/merchants/:id/bulk_discounts/:id', type: :feature do
+RSpec.describe 'bulk_discounts/:id', type: :feature do
   before(:each) do
     test_data
   end
 
   describe 'When I visit a discounts show page' do
     it 'I see the discounts name' do
-      visit merchant_bulk_discount_path(@merchant, @discount1)
+      visit bulk_discount_path(@discount1)
 
       expect(page).to have_content("#{@discount1.name} discount")
     end
