0d1c3b6f7983e383feadf17a5b21b33a612ca298
Thomas Hawley <thawley22@turing.edu>
2023-04-24 19:36:03 -0600

    feat: Add #applied_discount method to invoice item model

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -3,4 +3,11 @@ class InvoiceItem < ApplicationRecord
   belongs_to :invoice
 
   enum status: { 'pending': 0, 'packaged': 1, 'shipped': 2 }
+
+  def applied_discount
+    item.merchant.bulk_discounts
+    .where("#{self.quantity} >= bulk_discounts.quantity_threshold")
+    .order("bulk_discounts.percent_discount DESC")
+    .first
+  end
 end
