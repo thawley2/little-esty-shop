090a9b6eea4304228ce450abbbc5180c67985eab
Thomas Hawley <thawley22@turing.edu>
2023-04-25 16:09:21 -0600

    feat: update merchant table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_21_213523) do
+ActiveRecord::Schema.define(version: 2023_04_25_220241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2023_04_21_213523) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "enabled", default: true
+    t.boolean "active_discount", default: false
   end
 
   create_table "transactions", force: :cascade do |t|
